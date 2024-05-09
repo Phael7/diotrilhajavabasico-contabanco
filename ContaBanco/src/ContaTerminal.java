1a2bcf0eb08bad56ba9416e5277803d9ee2fd728
Raphael Pereira de Araujo <145080836+Phael7@users.noreply.github.com>
2024-05-09 16:12:39 -0300

    Conta-Banco - Projeto finalizado

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite os 3 primeiros número da Agência !");
+        int Numero = scanner.nextInt();
+
+        System.out.println("Por favor, digite os 3 últimos número da Agência !");
+        String Agencia = scanner.next();
+
+        System.out.println("Por favor, digite o seu nome completo !");
+        String Nome = scanner.next();
+
+        System.out.println("Por favor, digite o seu saldo !");
+        double Saldo = scanner.nextDouble();
+
+        System.out.println("Olá" + Nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + Agencia + ", conta " + Numero + " e seu saldo " + Saldo + " já está disponível para saque");
+
+        scanner.close();
     }
 }
